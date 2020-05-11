e927fb38a2e836a85b0ce90775daf8a23fb80b69
d3xt3r-grv <grvarora3011@gmail.com>
2020-05-11 19:20:03 +0530

    upload API working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const indexController = require('../controllers/indexController');
-
+const uploadController = require('../controllers/uploadController')
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.index);
 
+router.post('/uploadDocument', uploadController.upload);
 
 module.exports = router;
