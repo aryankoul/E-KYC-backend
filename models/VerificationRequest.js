58a2fb4fe5025834e81a9295b57935918244159d
d3xt3r-grv <grvarora3011@gmail.com>
2020-05-16 13:15:44 +0530

    generate OTP

---
+++ models/VerificationRequest.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const requestSchema = new mongoose.Schema({
+  verifierAddress: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  signature: {
+    type: String,
+  },
+  email:{
+      type: String,
+  },
+});
+
+module.exports = mongoose.model('VerificationRequest', requestSchema);
